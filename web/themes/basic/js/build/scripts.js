fa4c3d4e9308d449f1654e18e259b08aca78bb6b
Ptrhnk <petr.nahak@gmail.com>
2026-04-15 15:38:11 +0100

    allow script in theme, add script to translate tour date table headings

--- web/themes/basic/js/build/scripts.js
+++ web/themes/basic/js/build/scripts.js
@@ -1,2 +1,67 @@
-"use strict";!function(n,o){n.behaviors.basic={attach:function(){o(window).on("load",function(){}),o(window).on("resize",function(){}),o(window).on("scroll",function(){})}}}(Drupal,jQuery);
-//# sourceMappingURL=scripts.js.map
+"use strict";
+(function (Drupal, $) {
+  function localizeTourTableHeaders(context) {
+    var lang = $("html").attr("lang") || "";
+    var path = window.location.pathname || "";
+    var isSk = lang.indexOf("sk") === 0;
+    var isTourPage = path.indexOf("/tour") !== -1 || path.indexOf("/koncerty") !== -1 || /\/node\/59(?:\/|$)/.test(path);
+    var selector = "#block-views-block-tour-dates-block-1 table thead th, #block-views-block-tour-dates-block-2 table thead th";
+
+    console.log("[tour-localize] attach start", {
+      lang: lang,
+      path: path,
+      isSk: isSk,
+      isTourPage: isTourPage
+    });
+
+    if (!isSk || !isTourPage) {
+      console.log("[tour-localize] skip: language/path condition not met");
+      return;
+    }
+
+    var translations = {
+      "Tour date": "Datum",
+      "City": "Mesto",
+      "Club": "Klub",
+      "Project": "Projekt",
+      "Tickets": "Vstupenky"
+    };
+
+    var $headers = $(selector, context);
+    console.log("[tour-localize] headers found in context", $headers.length, context);
+
+    $headers.each(function () {
+      var $th = $(this);
+      var current = $.trim($th.text());
+
+      console.log("[tour-localize] inspecting header", {
+        current: current,
+        alreadyLocalized: !!$th.data("tourHeaderLocalized"),
+        blockId: $th.closest("[id]").attr("id") || null
+      });
+
+      if ($th.data("tourHeaderLocalized")) {
+        console.log("[tour-localize] skip: already localized", current);
+        return;
+      }
+
+      if (translations[current]) {
+        console.log("[tour-localize] translating", current, "->", translations[current]);
+        $th.text(translations[current]);
+      } else {
+        console.log("[tour-localize] no translation mapping for", current);
+      }
+      $th.data("tourHeaderLocalized", true);
+    });
+  }
+
+  Drupal.behaviors.basic = {
+    attach: function (context) {
+      localizeTourTableHeaders(context);
+
+      $(window).on("load", function () {});
+      $(window).on("resize", function () {});
+      $(window).on("scroll", function () {});
+    }
+  };
+})(Drupal, jQuery);
--- web/themes/basic/js/source/scripts.js
+++ web/themes/basic/js/source/scripts.js
@@ -11,27 +11,85 @@
 // - https://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function (Drupal, $) {
-  'use strict';
+  "use strict";
+
+  function localizeTourTableHeaders(context) {
+    var lang = $("html").attr("lang") || "";
+    var path = window.location.pathname || "";
+    var isSk = lang.indexOf("sk") === 0;
+    var isTourPage =
+      path.indexOf("/tour") !== -1 || path.indexOf("/koncerty") !== -1 || /\/node\/59(?:\/|$)/.test(path);
+    var selector =
+      "#block-views-block-tour-dates-block-1 table thead th, #block-views-block-tour-dates-block-2 table thead th";
+
+    console.log("[tour-localize] attach start", {
+      lang: lang,
+      path: path,
+      isSk: isSk,
+      isTourPage: isTourPage,
+    });
+
+    if (!isSk || !isTourPage) {
+      console.log("[tour-localize] skip: language/path condition not met");
+      return;
+    }
+
+    var translations = {
+      "Tour date": "Datum",
+      City: "Mesto",
+      Club: "Klub",
+      Project: "Projekt",
+      Tickets: "Vstupenky",
+    };
+
+    var $headers = $(selector, context);
+    console.log("[tour-localize] headers found in context", $headers.length, context);
+
+    $headers.each(
+      function () {
+        var $th = $(this);
+        var current = $.trim($th.text());
+
+        console.log("[tour-localize] inspecting header", {
+          current: current,
+          alreadyLocalized: !!$th.data("tourHeaderLocalized"),
+          blockId: $th.closest("[id]").attr("id") || null,
+        });
+
+        if ($th.data("tourHeaderLocalized")) {
+          console.log("[tour-localize] skip: already localized", current);
+          return;
+        }
+
+        if (translations[current]) {
+          console.log("[tour-localize] translating", current, "->", translations[current]);
+          $th.text(translations[current]);
+        } else {
+          console.log("[tour-localize] no translation mapping for", current);
+        }
+        $th.data("tourHeaderLocalized", true);
+      },
+    );
+  }
 
   // To understand behaviors, see https://www.drupal.org/node/2269515
   Drupal.behaviors.basic = {
     attach: function (context, settings) {
+      localizeTourTableHeaders(context);
 
       // Execute code once the DOM is ready. $(handler) not required
       // within Drupal.behaviors.
-      $(window).on('load', function () {
+      $(window).on("load", function () {
         // Execute code once the window is fully loaded.
       });
 
-      $(window).on('resize', function () {
+      $(window).on("resize", function () {
         // Execute code when the window is resized.
       });
 
-      $(window).on('scroll', function () {
+      $(window).on("scroll", function () {
         // Execute code when the window scrolls.
       });
-
-    }
+    },
   };
-
 })(Drupal, jQuery);
